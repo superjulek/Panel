7640ac063ec88dedf062a27958182ac6154ffa2a
Juliusz Neuman <superjulek@interia.pl>
2020-12-11 20:39:37 +0100

    Set input pins

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -32,7 +32,7 @@ void MX_I2C1_Init(void)
 {
 
   hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x0050174F;
+  hi2c1.Init.Timing = 0x00B03FDB;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
@@ -56,9 +56,6 @@ void MX_I2C1_Init(void)
   {
     Error_Handler();
   }
-  /** I2C Enable Fast Mode Plus
-  */
-  HAL_I2CEx_EnableFastModePlus(I2C_FASTMODEPLUS_I2C1);
 
 }
 /* I2C2 init function */
